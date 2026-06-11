d0b3ad7680fbe20d476ef0c2099c5cb3f066f938
Adrian <spad1011@h-ka.de>
2026-06-11 18:40:20 +0200

    version bump

--- app/src/main/java/com/example/ebusiness/ui/theme/Theme.kt
+++ app/src/main/java/com/example/ebusiness/ui/theme/Theme.kt
@@ -1,6 +1,5 @@
 package com.example.ebusiness.ui.theme
 
-import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
